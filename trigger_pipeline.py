22432ccf66e4dc283d5c0cb555c9e815b94aa58e
Farhan Ghaffar <farhan.ghffar@gmail.com>
2023-10-13 11:08:51 -0700

    Update trigger_pipeline.py

--- trigger_pipeline.py
+++ trigger_pipeline.py
@@ -12,8 +12,8 @@ main_branch = 'main'
 
 # github_access_token = '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'
 g = Github(os.environ.get('GITHUB_TOKEN'))
-print(os.environ.get('GITHUB_TOKEN'))
-# repo = g.get_user(owner).get_repo(repo_name)
+# print(os.environ.get('GITHUB_TOKEN'))
+repo = g.get_user(owner).get_repo(repo_name)
 
 # workflow_runs = repo.get_workflow_runs(event = 'queued')
 
