ae07a720f5b7dc79582f4d62ac8e0e592b618d21
Farhan Ghaffar <farhan.ghffar@gmail.com>
2023-10-13 11:07:13 -0700

    Update trigger_pipeline.py

--- trigger_pipeline.py
+++ trigger_pipeline.py
@@ -11,11 +11,11 @@ workflow_id = '72571241'
 main_branch = 'main'
 
 # github_access_token = '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'
-g = Github(os.environ.get('GITHUB_TOKEN'))
+# g = Github(os.environ.get('GITHUB_TOKEN'))
+print(os.environ.get('GITHUB_TOKEN'))
+# repo = g.get_user(owner).get_repo(repo_name)
 
-repo = g.get_user(owner).get_repo(repo_name)
+# workflow_runs = repo.get_workflow_runs(event = 'queued')
 
-workflow_runs = repo.get_workflow_runs(event = 'queued')
-
-if workflow_runs.totalCount == 0:
-    repo.get_workflow(workflow_id).create_dispatch(ref=main_branch)
+# if workflow_runs.totalCount == 0:
+#     repo.get_workflow(workflow_id).create_dispatch(ref=main_branch)
