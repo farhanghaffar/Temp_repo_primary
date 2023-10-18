38785beb43fcfd316f0982d249ad9933355050f1
Farhan Ghaffar <farhan.ghffar@gmail.com>
2023-10-18 11:04:42 -0700

    Update trigger_secondary.js

--- trigger_secondary.js
+++ trigger_secondary.js
@@ -1,30 +1,33 @@
-const { Octokit } = require("@octokit/rest");
 
-const owner = 'farhanghaffar';
-const repo = 'temp_repo_secondary';
-const workflowId = '72571241';
-const mainBranch = 'main';
 
-const octokit = new Octokit({
-  auth: process.env.GITHUB_TOKEN,
-});
+const { Octokit } = require('@octokit/rest');
 
-async function createWorkflowDispatch() {
-  const { data: workflowRuns } = await octokit.actions.listWorkflowRuns({
-    owner,
-    repo,
-    event: 'push',
-    branch: mainBranch,
-  });
-
-  if (workflowRuns.total_count === 0) {
-    await octokit.actions.createWorkflowDispatch({
-      owner,
-      repo,
-      workflow_id: workflowId,
-      ref: mainBranch,
+(async () => {
+  try {
+    const octokit = new Octokit({
+      auth: process.env.GITHUB_TOKEN,
     });
-  }
-}
 
-createWorkflowDispatch();
+    const owner = 'farhanghaffar'; // Replace with the actual owner of the secondary repository
+    const repo = 'temp_repo_secondary'; // Replace with the actual name of the secondary repository
+    const workflowId = '72571241'; // Replace with the ID of the workflow in the secondary repository
+    const mainBranch = 'main'; // Replace with the name of the main branch in the secondary repository
+
+    const { data: workflowRuns } = await octokit.actions.listWorkflowRuns({
+      owner: owner,
+      repo: repo,
+      status: 'queued',
+    });
+
+    if (workflowRuns.total_count === 0) {
+      await octokit.actions.createWorkflowDispatch({
+        owner: owner,
+        repo: repo,
+        workflow_id: workflowId,
+        ref: mainBranch,
+      });
+    }
+  } catch (error) {
+    console.error('An error occurred: ', error);
+  }
+})();
