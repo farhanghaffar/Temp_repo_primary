df580ac28bd105e5e9fee83c988e07cf7e0e711e
Farhan Ghaffar <farhan.ghffar@gmail.com>
2023-10-18 11:08:43 -0700

    Update trigger_secondary.js

--- trigger_secondary.js
+++ trigger_secondary.js
@@ -1,33 +1,15 @@
 
 
 const { Octokit } = require('@octokit/rest');
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN
+})
 
-(async () => {
-  try {
-    const octokit = new Octokit({
-      auth: process.env.GITHUB_TOKEN,
-    });
-
-    const owner = 'farhanghaffar'; // Replace with the actual owner of the secondary repository
-    const repo = 'temp_repo_secondary'; // Replace with the actual name of the secondary repository
-    const workflowId = '72571241'; // Replace with the ID of the workflow in the secondary repository
-    const mainBranch = 'main'; // Replace with the name of the main branch in the secondary repository
-
-    const { data: workflowRuns } = await octokit.actions.listWorkflowRuns({
-      owner: owner,
-      repo: repo,
-      status: 'queued',
-    });
-
-    if (workflowRuns.total_count === 0) {
-      await octokit.actions.createWorkflowDispatch({
-        owner: owner,
-        repo: repo,
-        workflow_id: workflowId,
-        ref: mainBranch,
-      });
-    }
-  } catch (error) {
-    console.error('An error occurred: ', error);
+await octokit.request('POST /repos/{owner}/{repo}/actions/jobs/{job_id}/rerun', {
+  owner: 'farhanghaffar',
+  repo: 'temp_repo_secondary',
+  job_id: '72571241',
+  headers: {
+    'X-GitHub-Api-Version': '2022-11-28'
   }
-})();
+})
