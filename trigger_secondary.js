8cfaa9ea6cb844d934cfd90536c9243a6852238c
Farhan Ghaffar <farhan.ghffar@gmail.com>
2023-10-19 04:07:10 -0700

    Update trigger_secondary.js

--- trigger_secondary.js
+++ trigger_secondary.js
@@ -1,15 +1,23 @@
-
-
 const { Octokit } = require('@octokit/rest');
-const octokit = new Octokit({
-  auth: process.env.GITHUB_TOKEN
-})
 
-await octokit.request('POST /repos/{owner}/{repo}/actions/jobs/{job_id}/rerun', {
-  owner: 'farhanghaffar',
-  repo: 'temp_repo_secondary',
-  job_id: '72571241',
-  headers: {
-    'X-GitHub-Api-Version': '2022-11-28'
+async function triggerRerun() {
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_TOKEN
+  });
+
+  try {
+    const response = await octokit.request('POST /repos/{owner}/{repo}/actions/jobs/{job_id}/rerun', {
+      owner: 'farhanghaffar',
+      repo: 'temp_repo_secondary',
+      job_id: '72571241',
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    });
+    console.log(response.data); // Assuming you want to log the response data
+  } catch (error) {
+    console.error('Error occurred:', error);
   }
-})
+}
+
+triggerRerun();
